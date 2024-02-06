5410d919ecb31c8bc98e60cf34ad01221bff82c7
Mckgare0717 <mckinnonrebello@gmail.com>
2024-02-06 12:55:17 +0000

    done help

--- src/components/health/HealthList.js
+++ src/components/health/HealthList.js
@@ -124,45 +124,54 @@ const HealthList = () => {
                 <h4>Components: </h4>
                 <div className="popup-group">
                   <label for="co">Carbon Monoxide :<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Carbon Monoxide</h3>Carbon monoxide (chemical formula CO) is a poisonous, flammable gas that is colorless, odorless, tasteless, and slightly less dense than air.
-                   Carbon monoxide consists of one carbon atom and one oxygen atom connected by a triple bond. It is the simplest carbon oxide. 
-                   In coordination complexes, the carbon monoxide ligand is called carbonyl. It is a key ingredient in many processes in industrial chemistry.[5]
+                    Carbon monoxide consists of one carbon atom and one oxygen atom connected by a triple bond. It is the simplest carbon oxide.
+                    In coordination complexes, the carbon monoxide ligand is called carbonyl. It is a key ingredient in many processes in industrial chemistry.[5]
                     [7]
                     .</p></Popup></label>
                   <h4 id="co">{health.list[0].components.co}μg/m3</h4>
                   <progress id="co" value={health.list[0].components.co} max="300"> {health.list[0].components.co}% </progress>
                 </div>
                 <div className="popup-group">
-                  <label for="nh3">Nitrogen Monoxide:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Nitrogen Monoxide</h3></p></Popup><HelpCircle /></label>
+                  <label for="nh3">Ammonia:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Ammonia</h3>Ammonia is an inorganic chemical compound of nitrogen and hydrogen with the formula NH3. A stable binary hydride and the simplest pnictogen hydride, ammonia is a colourless gas with a distinct pungent smell. Biologically, it is a common nitrogenous waste, and it contributes significantly to the nutritional needs of terrestrial organisms by serving as a precursor to fertilisers.[12] Around 70% of ammonia produced industrially is used to make fertilisers[13] in various forms and composition, such as urea and diammonium phosphate.
+                    Ammonia in pure form is also applied directly into the soil.</p></Popup></label>
                   <h4 id="nh3">{health.list[0].components.nh3}μg/m3</h4>
                   <progress id="nh3" value={health.list[0].components.nh3} max="10"> {health.list[0].components.nh3}% </progress>
                 </div>
                 <div className="popup-group">
-                  <label for="no">Ammonia:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Ammonia</h3></p></Popup></label>
+                  <label for="no">Nitrogen Monoxide:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Nitrogen Monoxide</h3>Nitric oxide (nitrogen oxide or nitrogen monoxide[1]) is a colorless gas with the formula NO. It is one of the principal oxides of nitrogen. Nitric oxide is a free radical: it has an unpaired electron, which is sometimes denoted by a dot in its chemical formula (•N=O or •NO). Nitric oxide is also a heteronuclear diatomic molecule,
+                    a class of molecules whose study spawned early modern theories of chemical bonding.</p></Popup></label>
                   <h4 id="no">{health.list[0].components.no}μg/m3</h4>
                   <progress id="no" value={health.list[0].components.no} max="10"> {health.list[0].components.no}% </progress>
                 </div>
                 <div className="popup-group">
-                  <label for="no2">Nitrogen Dioxide:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Nitrogen Dioxide</h3></p></Popup></label>
+                  <label for="no2">Nitrogen Dioxide:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Nitrogen Dioxide</h3>Nitrogen dioxide is a chemical compound with the formula NO2. One of several nitrogen oxides, nitrogen dioxide is a reddish-brown gas. It is a paramagnetic, bent molecule with C2v point group symmetry. Industrially, NO2 is an intermediate in the synthesis of nitric acid, millions of tons of which are produced each year,
+                    primarily for the production of fertilizers.</p></Popup></label>
                   <h4 id="no2">{health.list[0].components.n2o}μg/m3</h4>
                   <progress id="no2" value={health.list[0].components.no2} max="100"> {health.list[0].components.no2}% </progress>
                 </div>
                 <div className="popup-group">
-                  <label for="o3">Ozone:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Ozone</h3></p></Popup></label>
+                  <label for="o3">Ozone:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Ozone</h3>Ozone (/ˈoʊzoʊn/) (or trioxygen) is an inorganic molecule with the chemical formula O
+                    3. It is a pale blue gas with a distinctively pungent smell. It is an allotrope of oxygen that is much less stable than the diatomic allotrope O
+                    2, breaking down in the lower atmosphere to O
+                    2 (dioxygen). Ozone is formed from dioxygen by the action of ultraviolet (UV) light and electrical discharges within the Earth's atmosphere. It is present in very low concentrations throughout the atmosphere, with its highest concentration high in the ozone layer of the stratosphere,
+                    which absorbs most of the Sun's ultraviolet (UV) radiation.</p></Popup></label>
                   <h4 id="o3">{health.list[0].components.o3}μg/m3</h4>
                   <progress id="o3" value={health.list[0].components.o3} max="100"> {health.list[0].components.o3}% </progress>
                 </div>
                 <div className="popup-group">
-                  <label for="pm2_5">Fine Particles Matter:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Fine Particles Matter</h3></p></Popup></label>
+                  <label for="pm2_5">Fine Particles Matter:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Fine Particles Matter</h3>Particulates or atmospheric particulate matter (see below for other names) are microscopic particles of solid or liquid matter suspended in the air. The term aerosol commonly refers to the particulate/air mixture, as opposed to the particulate matter alone.[1] Sources of particulate matter can be natural or anthropogenic.[2] They have impacts on climate and precipitation that adversely affect human health, 
+                  in ways additional to direct inhalation.</p></Popup></label>
                   <h4 id="pm2_5">{health.list[0].components.pm2_5}μg/m3</h4>
                   <progress id="pm2_5" value={health.list[0].components.pm2_5} max="10"> {health.list[0].components.pm2_5}% </progress>
                 </div>
                 <div className="popup-group">
-                  <label for="pm10">Coarse Particles Matter:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Coarse Particles Matter</h3></p></Popup></label>
+                  <label for="pm10">Coarse Particles Matter:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Coarse Particles Matter</h3>Particulates or atmospheric particulate matter (see below for other names) are microscopic particles of solid or liquid matter suspended in the air. The term aerosol commonly refers to the particulate/air mixture, as opposed to the particulate matter alone.[1] Sources of particulate matter can be natural or anthropogenic.[2] They have impacts on climate and precipitation that adversely affect human health, in ways additional to direct inhalation.</p></Popup></label>
                   <h4 id="pm10">{health.list[0].components.pm10}μg/m3</h4>
                   <progress id="pm10" value={health.list[0].components.pm10} max="10"> {health.list[0].components.pm10}% </progress>
                 </div>
                 <div className="popup-group">
-                  <label for="so2">Sulpher dioxide:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Sulpher dioxide</h3></p></Popup></label>
+                  <label for="so2">Sulpher dioxide:<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Sulpher dioxide</h3>Sulfur dioxide (IUPAC-recommended spelling) or sulphur dioxide (traditional Commonwealth English) is the chemical compound with the formula SO
+2. It is a toxic gas responsible for the odor of burnt matches. It is released naturally by volcanic activity and is produced as a by-product of copper extraction and the burning of sulfur-bearing fossil fuels.[8] It was known to alchemists as "volatile spirit of sulfur" since at least 16th century.[9]</p></Popup></label>
                   <h4 id="so2">{health.list[0].components.so2}μg/m3</h4>
                   <progress id="so2" value={health.list[0].components.so2} max="10"> {health.list[0].components.so2}% </progress>
                 </div>
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -12,6 +12,7 @@ import HealthList from "../../components/health/HealthList";
 import { AuthContext } from "../../components/ActionContext/ActionContext";
 import { json } from "react-router-dom";
 import CheckBox from "../../components/checkbox/CheckBox"
+import { HelpCircle } from "lucide-react";
 
 
 
@@ -88,8 +89,8 @@ const Home = () => {
 
 
       {user ? <h1>Welcome {user.name}</h1> : null}
-      
-      
+
+
 
 
 
@@ -102,12 +103,12 @@ const Home = () => {
 
           {weatherData && (
             <div className="currentWeather">
-              <CheckBox text="Imperial" whenChecked={units==="imperial"} onchange={()=>setUnits(units==="metric"?"imperial":"metric")}/>
+              <CheckBox text="Imperial" whenChecked={units === "imperial"} onchange={() => setUnits(units === "metric" ? "imperial" : "metric")} />
               <h2>Current Weather</h2>
               <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt={weatherData.weather[0].description} />
               <div className="info-cont">
                 <p><MapPin />: {weatherData?.name}</p>
-                <p><Thermometer />: {weatherData.main.temp}{units=="imperial"?<>°F</>:<>°C</>}</p>
+                <p><Thermometer />: {weatherData.main.temp}{units == "imperial" ? <>°F</> : <>°C</>}</p>
                 <p><BookA />: {weatherData.weather[0].description}</p>
               </div>
             </div>
@@ -122,13 +123,25 @@ const Home = () => {
               <h2><SprayCan />: {health.list[0].main.aqi}</h2>
               <h3><Component />:</h3>
               <div className="components-cont">
-                <h4>co: {health.list[0].components.co}</h4>
-                <h4>nh3: {health.list[0].components.nh3}</h4>
-                <h4>no: {health.list[0].components.no}</h4>
-                <h4>o3: {health.list[0].components.o3}</h4>
-                <h4>pm25: {health.list[0].components.pm2_5}</h4>
-                <h4>pm10: {health.list[0].components.pm10}</h4>
-                <h4>so2: {health.list[0].components.so2}</h4>
+                <h4><span>Carbon Monoxide <Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Carbon Monoxide</h3>Carbon monoxide (chemical formula CO) is a poisonous, flammable gas that is colorless, odorless, tasteless, and slightly less dense than air.
+                  Carbon monoxide consists of one carbon atom and one oxygen atom connected by a triple bond. It is the simplest carbon oxide.
+                  In coordination complexes, the carbon monoxide ligand is called carbonyl. It is a key ingredient in many processes in industrial chemistry.[5]
+                  [7]
+                  .</p></Popup></span>{health.list[0].components.co}μg/m3</h4>
+                <h4><span>Ammonia<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Ammonia</h3>Ammonia is an inorganic chemical compound of nitrogen and hydrogen with the formula NH3. A stable binary hydride and the simplest pnictogen hydride, ammonia is a colourless gas with a distinct pungent smell. Biologically, it is a common nitrogenous waste, and it contributes significantly to the nutritional needs of terrestrial organisms by serving as a precursor to fertilisers.[12] Around 70% of ammonia produced industrially is used to make fertilisers[13] in various forms and composition, such as urea and diammonium phosphate.
+                  Ammonia in pure form is also applied directly into the soil.</p></Popup> </span>{health.list[0].components.nh3}μg/m3</h4>
+                <h4><span>Nitrogen Monoxide<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Nitrogen Monoxide</h3>Nitric oxide (nitrogen oxide or nitrogen monoxide[1]) is a colorless gas with the formula NO. It is one of the principal oxides of nitrogen. Nitric oxide is a free radical: it has an unpaired electron, which is sometimes denoted by a dot in its chemical formula (•N=O or •NO). Nitric oxide is also a heteronuclear diatomic molecule,
+                  a class of molecules whose study spawned early modern theories of chemical bonding.</p></Popup> </span>{health.list[0].components.no}μg/m3</h4>
+                <h4><span>Ozone<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Ozone</h3>Ozone (/ˈoʊzoʊn/) (or trioxygen) is an inorganic molecule with the chemical formula O
+                  3. It is a pale blue gas with a distinctively pungent smell. It is an allotrope of oxygen that is much less stable than the diatomic allotrope O
+                  2, breaking down in the lower atmosphere to O
+                  2 (dioxygen). Ozone is formed from dioxygen by the action of ultraviolet (UV) light and electrical discharges within the Earth's atmosphere. It is present in very low concentrations throughout the atmosphere, with its highest concentration high in the ozone layer of the stratosphere,
+                  which absorbs most of the Sun's ultraviolet (UV) radiation.</p></Popup></span> {health.list[0].components.o3}μg/m3</h4>
+                <h4><span>Fine Particles Matter<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Fine Particles Matter</h3>Particulates or atmospheric particulate matter (see below for other names) are microscopic particles of solid or liquid matter suspended in the air. The term aerosol commonly refers to the particulate/air mixture, as opposed to the particulate matter alone.[1] Sources of particulate matter can be natural or anthropogenic.[2] They have impacts on climate and precipitation that adversely affect human health,
+                  in ways additional to direct inhalation.</p></Popup></span> {health.list[0].components.pm2_5}μg/m3</h4>
+                <h4><span>Coarse Particles Matter<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Coarse Particles Matter</h3>Particulates or atmospheric particulate matter (see below for other names) are microscopic particles of solid or liquid matter suspended in the air. The term aerosol commonly refers to the particulate/air mixture, as opposed to the particulate matter alone.[1] Sources of particulate matter can be natural or anthropogenic.[2] They have impacts on climate and precipitation that adversely affect human health, in ways additional to direct inhalation.</p></Popup></span> {health.list[0].components.pm10}μg/m3</h4>
+                <h4><span>Sulpher dioxide<Popup on={['hover', 'focus']} trigger={<HelpCircle />} position="right center"><p><h3>Sulpher dioxide</h3>Sulfur dioxide (IUPAC-recommended spelling) or sulphur dioxide (traditional Commonwealth English) is the chemical compound with the formula SO
+                  2. It is a toxic gas responsible for the odor of burnt matches. It is released naturally by volcanic activity and is produced as a by-product of copper extraction and the burning of sulfur-bearing fossil fuels.[8] It was known to alchemists as "volatile spirit of sulfur" since at least 16th century.[9]</p></Popup></span> {health.list[0].components.so2}μg/m3</h4>
               </div>
 
 
